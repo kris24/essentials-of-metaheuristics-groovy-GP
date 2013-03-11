c670e882e9ece3e62cf75b50d5e766322bd79660
mart2967 <mart2967@apollo.morris.umn.edu>
2013-03-11 14:15:25 -0500

    implementing getRandomNode

--- EssentialsOfMetaheuristics/src/treeGP/Node.groovy
+++ EssentialsOfMetaheuristics/src/treeGP/Node.groovy
@@ -3,57 +3,55 @@ package treeGP
 import java.util.Random
 
 class Node {
-    
-	Random rand = new Random()
+
+    Random rand = new Random()
     Node right
     Node left
-         
+
     def arityTwo = ['+', '-', '/', '*']
     def arityOne = ['sin', 'cos', 'tan']
     def value
     def arity
     def isOperator
     def isVar
-    
-    
-    def assignArity = { value ->    
+
+
+    def assignArity = { value ->
         if (arityTwo.contains(value)) {
             arity = 2
         } else if (arityOne.contains(value)){
             arity = 1
         } else {
             arity = 0
-        }  
+        }
     }
-    
-    
+
+
     //remove nullnode entirely, it doesn't seem to work
-    def Node(value, left = NullNode.instance(), right = NullNode.instance()) {
+    def Node(value) {
         this.value = value
         arity = assignArity(value)
         isOperator = (arity != 0)
         isVar = (!value.getClass() == String && !isOperator)
     }
-    
-    def isLeaf() {
-        return left == NullNode.instance() && right == NullNode.instance()
-    }
-    
+
+
+
     def evaluate() {
         if (arity == 0) {
             return value
         }
-        
+
         if (arity == 1) {
             // all arity-one functions have their child stored in the right node
             return evaluateArityOne(right)
         } else if (arity == 2) {
             return evaluateArityTwo(left, right)
         }
-      
+
     }
-    
-    
+
+
     def evaluateArityTwo(lNode, rNode) {
         switch (value) {
             case '+':
@@ -64,10 +62,10 @@ class Node {
                 return lNode.evaluate() * rNode.evaluate()
             case '/':
                 return lNode.evaluate() / rNode.evaluate()
-            
+
         }
     }
-    
+
     def evaluateArityOne(arg) {
         switch (value) {
             case 'sin':
@@ -78,90 +76,109 @@ class Node {
                 return Math.tan(arg.evaluate())
         }
     }
-    
-	def mutate() {
-		
-		if (arity == 2) {
-			value = arityTwo[rand.nextInt(4)]
-		} else if (arity == 1) {
-			value = arityOne[rand.nextInt(3)]
-		}
-		
-		
-		
-	}
-	
-	static Node crossover(Node a, Node b) {
-		
-		Node parentOne = a.clone()
-		Node parentTwo = b.clone()
-		
-		Node headOne = parentOne.getRandomNode()
-		Node headTwo = parentTwo.getRandomNode()
-		
-	}
-	
-	def getRandomNode(r = rand.nextInt() % size()) {
-		boolean returnLeaf = rand.nextInt(10) == 0
-		
-		//def r = rand.nextInt() % size()
-		if (left != null && r < left.size()) {
-			return left.getRandomNode(r)
-		} else if (right != null && r > left.size()) {
-			return right.getRandomNode(r - (right.size() ))
-		} else {
-			return this.clone()
-		}
-		
-	}
-	
-	def size() {
-		
-		if (arity == 0) {
-			return 1
-		} else if (arity == 1) {
-			return 1 + right.size()
-		} else {
-			return 1 + left.size() + right.size()
-		}
-		
-	}
-	
-	def depth() {
-		if (arity == 0) {
-			return 1
-		}
-		if (right.depth() > left.depth()) {
-			return right.depth() + 1
-		}
-		return left.depth() + 1
-	}
-	
-	def clone() {
-		Node head = new Node(value)
-		if (left != null) {
-			head.left = left.clone()
-		}
-		if (right != null) {
-			head.right = right.clone()
-		}
-		return head
-	}
-	
-	
-	
-    String toString(){
-		if (arity == 0) {
-			return value.toString()
-		} else if (arity == 1) {
-			return value.toString()+ "("+ right.toString() + ")"
-		}
-		return "(" + left.toString() + " " +  value.toString() + " " + right.toString() + ")"
+
+    def mutate() {
+
+        if (arity == 2) {
+            value = arityTwo[rand.nextInt(4)]
+        } else if (arity == 1) {
+            value = arityOne[rand.nextInt(3)]
+        }
+
+
+
     }
-	
+
+    static Node crossover(Node a, Node b) {
+
+        Node parentOne = a.clone()
+        Node parentTwo = b.clone()
+
+        Node headOne = parentOne.getRandomNode()
+        Node headTwo = parentTwo.getRandomNode()
+
+    }
+
+    //    def getRandomNode(r = (rand.nextInt() % size())+1) {
+    ////        boolean returnLeaf = rand.nextInt(10) == 0
+    //
+    //        if (left != null && r < left.size()) {
+    //            return left.getRandomNode(r)
+    //        } else if (right != null && r > right.size()) {
+    //            return right.getRandomNode(r - (right.size() ))
+    //        } else {
+    //            return this.clone()
+    //        }
+    //
+    //    }
+
+    def getRandomNode(r = (rand.nextInt() % depth()), iterations = 0, Node firstNode = this) {
+
+        int direction = rand.nextInt(2)
+        if (iterations == r) {
+            return this.clone()
+        } else if (direction == 0) {
+            if (left == null){
+                return firstNode.getRandomNode(r, iterations++, firstNode)
+            }
+            return left.getRandomNode(r, iterations++, firstNode)
+        } else if (direction == 1) {
+            if (right == null){
+                return firstNode.getRandomNode(r, iterations++, firstNode)
+            }
+            return right.getRandomNode(r, iterations++, firstNode)
+        }
+
+
+    }
+
+    def size() {
+
+        if (arity == 0) {
+            return 1
+        } else if (arity == 1) {
+            return 1 + right.size()
+        } else {
+            return 1 + left.size() + right.size()
+        }
+
+    }
+
+    def depth() {
+        if (arity == 0) {
+            return 1
+        }
+        if (right.depth() > left.depth()) {
+            return right.depth() + 1
+        }
+        return left.depth() + 1
+    }
+
+    def clone() {
+        Node head = new Node(value)
+        if (left != null) {
+            head.left = left.clone()
+        }
+        if (right != null) {
+            head.right = right.clone()
+        }
+        return head
+    }
+
+
+
+    String toString(){
+        if (arity == 0) {
+            return value.toString()
+        } else if (arity == 1) {
+            return value.toString()+ "("+ right.toString() + ")"
+        }
+        return "(" + left.toString() + " " +  value.toString() + " " + right.toString() + ")"
+    }
+
     def isNull() {
         return false
     }
-    
-    
+
+
 }
--- EssentialsOfMetaheuristics/test/treeGP/NodeSpecification.groovy
+++ EssentialsOfMetaheuristics/test/treeGP/NodeSpecification.groovy
@@ -3,57 +3,57 @@ package treeGP
 import spock.lang.Specification
 
 class NodeSpecification extends Specification {
-	def setupSpec() {
-		Node head = new Node ('+')
-		Node d1l = new Node ('-')
-		Node d1r = new Node ('*')
-		Node d2l1 = new Node (5)
-		Node d2r1 = new Node (2)
-		Node d2l2 = new Node (4)
-		Node d2r2 = new Node (4)
-		
-		head.left = d1l
-		head.right = d1r
-		d1l.left = d2l1
-		d1l.right = d2r1
-		d1r.left = d2l2
-		d1r.right = d2r2
-		
-		
-	}
-	
+    def setupSpec() {
+        Node head = new Node ('+')
+        Node d1l = new Node ('-')
+        Node d1r = new Node ('*')
+        Node d2l1 = new Node (5)
+        Node d2r1 = new Node (2)
+        Node d2l2 = new Node (4)
+        Node d2r2 = new Node (4)
+
+        head.left = d1l
+        head.right = d1r
+        d1l.left = d2l1
+        d1l.right = d2r1
+        d1r.left = d2l2
+        d1r.right = d2r2
+
+
+    }
+
     def 'test arity'() {
         when:
         Node add = new Node('+')
         Node sub = new Node('-')
         Node mul = new Node('*')
         Node divis = new Node('/')
-        
+
         Node sin = new Node('sin')
         Node cos = new Node('cos')
         Node tan = new Node('tan')
-        
+
         Node constant = new Node(5)
         then:
-        
+
         add.arity == 2
         sub.arity == 2
         mul.arity == 2
         divis.arity == 2
-        
+
         sin.arity == 1
         cos.arity == 1
         tan.arity == 1
-        
+
         constant.arity == 0
     }
-    
+
     def 'test addition'() {
         when:
         Node head = new Node('+')
         head.right = new Node(6)
         head.left = new Node(4)
-        
+
         then:
         head.evaluate() == 10
     }
@@ -63,7 +63,7 @@ class NodeSpecification extends Specification {
         Node head = new Node('-')
         head.left = new Node(4)
         head.right = new Node(6)
-        
+
         then:
         head.evaluate() == -2
     }
@@ -73,52 +73,52 @@ class NodeSpecification extends Specification {
         Node head = new Node('*')
         head.right = new Node(6)
         head.left = new Node(4)
-        
+
         then:
         head.evaluate() == 24
     }
-    
+
     def 'test division'() {
         when:
         Node head = new Node('/')
         head.left = new Node(6)
         head.right = new Node(2)
-        
-        
+
+
         then:
         head.evaluate() == 3
     }
-    
+
     def 'test sin'() {
         when:
         Node head = new Node('sin')
         head.right = new Node(Math.PI)
-        
-        
+
+
         then:
         ((long)head.evaluate()) == 0
     }
-    
+
     def 'test cos'() {
         when:
         Node head = new Node('cos')
         head.right = new Node(Math.PI)
-        
-        
+
+
         then:
         ((long)head.evaluate()) == -1
     }
-    
+
     def 'test tan'() {
         when:
         Node head = new Node('tan')
         head.right = new Node(Math.PI)
-        
-        
+
+
         then:
         ((long)head.evaluate()) == 0
     }
-    
+
     def 'test a larger tree'() {
         when:
         Node head = new Node ('+')
@@ -128,179 +128,184 @@ class NodeSpecification extends Specification {
         Node d2r1 = new Node (2)
         Node d2l2 = new Node (4)
         Node d2r2 = new Node (4)
-        
+
         head.left = d1l
         head.right = d1r
         d1l.left = d2l1
         d1l.right = d2r1
         d1r.left = d2l2
         d1r.right = d2r2
-        
+
         then:
         head.evaluate() == 19
     }
-	
-	def 'test toString'() {
-		
-		when:
-		Node head = new Node ('+')
-		Node d1l = new Node ('-')
-		Node d1r = new Node ('*')
-		Node d2l1 = new Node (5)
-		Node d2r1 = new Node (2)
-		Node d2l2 = new Node (4)
-		Node d2r2 = new Node (4)
-		head.left = d1l
-		head.right = d1r
-		d1l.left = d2l1
-		d1l.right = d2r1
-		d1r.left = d2l2
-		d1r.right = d2r2
-		
-		Node head2 = new Node('/')
-		Node l = new Node('sin')
-		Node r = new Node('cos')
-		Node lv = new Node(Math.PI)
-		Node rv = new Node(Math.PI)
-		head2.left = l
-		head2.right = r
-		l.right = lv
-		r.right = rv
-		
-		then:
-		head.toString() == "((5 - 2) + (4 * 4))"
-		head2.toString() == "(sin(3.141592653589793) / cos(3.141592653589793))"
-		
-	}
-	
-	def 'test cloning'() {
-		when:
-		Node head = new Node ('+')
-		Node d1l = new Node ('-')
-		Node d1r = new Node ('*')
-		Node d2l1 = new Node (5)
-		Node d2r1 = new Node (2)
-		Node d2l2 = new Node (4)
-		Node d2r2 = new Node (4)
-		head.left = d1l
-		head.right = d1r
-		d1l.left = d2l1
-		d1l.right = d2r1
-		d1r.left = d2l2
-		d1r.right = d2r2
-		
-		Node head2 = head.clone()
-		
-		then:
-		head != head2
-		head.toString() == head2.toString()
-		head.evaluate() == head2.evaluate()
-	}
-	
-	def 'test size'() {
-		when:
-		Node head = new Node ('+')
-		Node d1l = new Node ('-')
-		Node d1r = new Node ('*')
-		Node d2l1 = new Node (5)
-		Node d2r1 = new Node (2)
-		Node d2l2 = new Node (4)
-		Node d2r2 = new Node (4)
-		head.left = d1l
-		head.right = d1r
-		d1l.left = d2l1
-		d1l.right = d2r1
-		d1r.left = d2l2
-		d1r.right = d2r2
-		
-		then:
-		
-		head.size() == 7
-		d1l.size() == 3
-		d2l1.size() == 1
-	}
-	
-	def 'test depth'() {
-		when:
-		Node head = new Node ('+')
-		Node d1l = new Node ('-')
-		Node d1r = new Node ('*')
-		Node d2l1 = new Node (5)
-		Node d2r1 = new Node (2)
-		Node d2l2 = new Node (4)
-		Node d2r2 = new Node (4)
-		head.left = d1l
-		head.right = d1r
-		d1l.left = d2l1
-		d1l.right = d2r1
-		d1r.left = d2l2
-		d1r.right = d2r2
-		
-		then:
-		head.depth() == 3
-		d1l.depth() == 2
-		d2l1.depth() == 1
-		
-	}
-	
-	def 'test random nodes'() {
-		when:
-		Node head = new Node ('+')
-		Node d1l = new Node ('-')
-		Node d1r = new Node ('*')
-		Node d2l1 = new Node (5)
-		Node d2r1 = new Node (2)
-		Node d2l2 = new Node (4)
-		Node d2r2 = new Node (4)
-		head.left = d1l
-		head.right = d1r
-		d1l.left = d2l1
-		d1l.right = d2r1
-		d1r.left = d2l2
-		d1r.right = d2r2
-		
-		def tests = 100
-		def results = new ArrayList<String>()
-		
-		then:
-		tests.times {
-			results.add(head.getRandomNode().toString())
-			//there are 7 possibilities, 6 unique
-			
-			//println(head.getRandomNode())
-		}
-		def timesWhole
-		def timesLeft
-		def timesRight
-		def timesFive
-		def timesTwo
-		def timesFour //twice as significant
-		
-		for(item in results) {
-			println(item)
-			switch(item) {
-				case "((5 - 2) + (4 * 4))":
-				timesWhole++
-				case "(5 - 2)":
-				timesLeft++
-				case "(4 * 4)":
-				timesRight++
-				case "5":
-				timesFive++
-				case "2":
-				timesTwo++
-				case "4":
-				timesFour++
-				default:
-				println("Something went wrong")
-			}
-		}
-		
-		println("Percentage ((5 - 2) + (4 * 4)) occured: " + (timesWhole / tests))
-		println("Percentage (5 - 2) occured: " + (timesLeft / tests))
-		println("Percentage (4 * 4) occured: " + (timesRight / tests))
-		println("Percentage 5 occured: " + (timesFive / tests))
-		println("Percentage 2 occured: " + (timesTwo / tests))
-		println("Percentage 4 occured: " + (timesFour / tests))
-	}
+
+    def 'test toString'() {
+
+        when:
+        Node head = new Node ('+')
+        Node d1l = new Node ('-')
+        Node d1r = new Node ('*')
+        Node d2l1 = new Node (5)
+        Node d2r1 = new Node (2)
+        Node d2l2 = new Node (4)
+        Node d2r2 = new Node (4)
+        head.left = d1l
+        head.right = d1r
+        d1l.left = d2l1
+        d1l.right = d2r1
+        d1r.left = d2l2
+        d1r.right = d2r2
+
+        Node head2 = new Node('/')
+        Node l = new Node('sin')
+        Node r = new Node('cos')
+        Node lv = new Node(Math.PI)
+        Node rv = new Node(Math.PI)
+        head2.left = l
+        head2.right = r
+        l.right = lv
+        r.right = rv
+
+        then:
+        head.toString() == "((5 - 2) + (4 * 4))"
+        head2.toString() == "(sin(3.141592653589793) / cos(3.141592653589793))"
+
+    }
+
+    def 'test cloning'() {
+        when:
+        Node head = new Node ('+')
+        Node d1l = new Node ('-')
+        Node d1r = new Node ('*')
+        Node d2l1 = new Node (5)
+        Node d2r1 = new Node (2)
+        Node d2l2 = new Node (4)
+        Node d2r2 = new Node (4)
+        head.left = d1l
+        head.right = d1r
+        d1l.left = d2l1
+        d1l.right = d2r1
+        d1r.left = d2l2
+        d1r.right = d2r2
+
+        Node head2 = head.clone()
+
+        then:
+        head != head2
+        head.toString() == head2.toString()
+        head.evaluate() == head2.evaluate()
+    }
+
+    def 'test size'() {
+        when:
+        Node head = new Node ('+')
+        Node d1l = new Node ('-')
+        Node d1r = new Node ('*')
+        Node d2l1 = new Node (5)
+        Node d2r1 = new Node (2)
+        Node d2l2 = new Node (4)
+        Node d2r2 = new Node (4)
+        head.left = d1l
+        head.right = d1r
+        d1l.left = d2l1
+        d1l.right = d2r1
+        d1r.left = d2l2
+        d1r.right = d2r2
+
+        then:
+
+        head.size() == 7
+        d1l.size() == 3
+        d2l1.size() == 1
+    }
+
+    def 'test depth'() {
+        when:
+        Node head = new Node ('+')
+        Node d1l = new Node ('-')
+        Node d1r = new Node ('*')
+        Node d2l1 = new Node (5)
+        Node d2r1 = new Node (2)
+        Node d2l2 = new Node (4)
+        Node d2r2 = new Node (4)
+        head.left = d1l
+        head.right = d1r
+        d1l.left = d2l1
+        d1l.right = d2r1
+        d1r.left = d2l2
+        d1r.right = d2r2
+
+        then:
+        head.depth() == 3
+        d1l.depth() == 2
+        d2l1.depth() == 1
+
+    }
+
+    def 'test random nodes'() {
+        when:
+        Node head = new Node ('+')
+        Node d1l = new Node ('-')
+        Node d1r = new Node ('*')
+        Node d2l1 = new Node (5)
+        Node d2r1 = new Node (2)
+        Node d2l2 = new Node (4)
+        Node d2r2 = new Node (4)
+        head.left = d1l
+        head.right = d1r
+        d1l.left = d2l1
+        d1l.right = d2r1
+        d1r.left = d2l2
+        d1r.right = d2r2
+
+        def tests = 100
+        def timesWhole = 0
+        def timesLeft = 0
+        def timesRight = 0
+        def timesFive = 0
+        def timesTwo = 0
+        def timesFour = 0 //twice as significant
+        
+        tests.times {
+            Node t = head.getRandomNode()
+            //there are 7 possibilities, 6 unique
+
+            switch(t.toString()) {
+                case "((5 - 2) + (4 * 4))":
+                    timesWhole++
+                    break
+                case "(5 - 2)":
+                    timesLeft++
+                    break
+                case "(4 * 4)":
+                    timesRight++
+                    break
+                case "5":
+                    timesFive++
+                    break
+                case "2":
+                    timesTwo++
+                    break
+                case "4":
+                    timesFour++
+                    break
+                default:
+                    println("Something went wrong")
+            }
+        }
+        then:
+        
+
+
+        
+
+        println("Percentage ((5 - 2) + (4 * 4)) occured: " + (timesWhole / tests)*100 + "%")
+        println("Percentage (5 - 2) occured: " + (timesLeft / tests)*100 + "%")
+        println("Percentage (4 * 4) occured: " + (timesRight / tests)*100 + "%")
+        println("Percentage 5 occured: " + (timesFive / tests)*100 + "%")
+        println("Percentage 2 occured: " + (timesTwo / tests)*100 + "%")
+        println("Percentage 4 occured: " + (timesFour / tests)*100 + "%")
+        (timesWhole + timesLeft + timesRight + timesFive + timesTwo + timesFour)/tests == 1
+    }
 }
