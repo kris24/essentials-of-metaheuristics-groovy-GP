ee71c9e55943e4d9d85edd9a529b75c355b15d60
kristofor <kristofor@kristofor-VirtualBox>
2013-04-15 17:17:11 -0500

    Still working on crossover

--- EssentialsOfMetaheuristics/src/applications/robocode/RoboCrossover.groovy
+++ EssentialsOfMetaheuristics/src/applications/robocode/RoboCrossover.groovy
@@ -1,25 +1,52 @@
 package applications.robocode
 
 class RoboCrossover {
-	def codeArray = []
-	def crossoverArray = []
-	def Random random = new Random()
-	def code = ""
 	
-	def makeArray(code) {
-		codeArray = code.split("  ")
+	def Random random = new Random()
+	
+	def crossover(str1, str2) {
+		def items = []
+		
+		System.out.println("Initial Strings")
+		System.out.println(str1)
+		//System.out.println(str2)
+		
+		def arr1 = makeArray(str1)
+		def arr2 = makeArray(str2)
+		
+		System.out.println("Converted to Arrays")
+		System.out.println(arr1)
+		//System.out.println(arr2)		
+		
+		items = onePointCrossover(arr1,arr2)
+		
+		System.out.println("After Crossover")
+		System.out.println(items[0])
+		//System.out.println(items[1])
+		
+		str1 = backToString(items[0])
+		str2 = backToString(items[1])
+		
+		System.out.println("Final Strings")
+		System.out.println(str1)
+		System.out.println(str2)
+	}
+	
+	def makeArray(str) {
+		def codeArray = []
+		codeArray = str.split("  ")
 		return codeArray
 	}
 	//borrowed from Crossovers operator
 	def onePointCrossover = { father, mother, crossoverPoint = random.nextInt(father.size()) ->
 		[
-			crossoverArray.add(father[0..<crossoverPoint] + mother[crossoverPoint..<mother.size()]),
-			crossoverArray.add(mother[0..<crossoverPoint] + father[crossoverPoint..<father.size()])
+			father[0..<crossoverPoint] + mother[crossoverPoint..<mother.size()],
+			mother[0..<crossoverPoint] + father[crossoverPoint..<father.size()]
 		]
-		return crossoverArray
 	}
 	
 	def backToString(array){
+		def code = ""
 		for ( item in array ) {
 			code = code + item
 		}
--- EssentialsOfMetaheuristics/test/applications/robocode/TestRoboCrossover.groovy
+++ EssentialsOfMetaheuristics/test/applications/robocode/TestRoboCrossover.groovy
@@ -5,6 +5,7 @@ import spock.lang.Specification
 class TestRoboCrossover extends Specification {
     
 	def roboCrossover
+	def roboCrossover2
 	
 	def items = []
 	
@@ -12,10 +13,20 @@ class TestRoboCrossover extends Specification {
 	
 	def setup() {
 		roboCrossover = new RoboCrossover()
+	
 	}
 
-
-    def "Confirm that string split works"() {
+	def "test crossover operator"() {
+		when:
+		def string1 = "setAhead(90);  turnRight(360);  setBack(180);  "
+		def string2 = "if(getEnergy()<getX()){ turnLeft(360); }  turnLeft(getVelocity());  ahead(90);  stop(); "
+		roboCrossover.crossover(string1,string2)
+		
+		then:
+		System.out.println("TEST: \n" + string1 + "\n" + string2)
+	}
+    
+	def "Confirm that string split works"() {
 		when:
 		def string1 = "setAhead(90);  turnRight(360);  setBack(180);"
 		def result1 = roboCrossover.makeArray(string1)
@@ -28,39 +39,41 @@ class TestRoboCrossover extends Specification {
 	
 	def "Crossover"() {
 		when:
-		def string1 = "setAhead(90);  turnRight(360);  setBack(180);"
+		def string1 = "setAhead(90);  turnRight(360);  setBack(180);  "
 		def string2 = "if(getEnergy()<getX()){ turnLeft(360); }  turnLeft(getVelocity());  ahead(90);  stop(); "
 		def result1 = roboCrossover.makeArray(string1)
 		def result2 = roboCrossover.makeArray(string2)
-		
-		items = roboCrossover.onePointCrossover(result1,result2)
-		result1 = items[0]
-		result2 = items[1]
-		
+				
 		then:
-		for ( i in result1 ) {
-		System.out.println(result1[i])
+		
+		for ( i in 0..5 ) {
+			roboCrossover.onePointCrossover(result1,result2)
+			//System.out.println(result1 )
+			//System.out.println(result2)
 		}
-		for ( i in result2 ) {
-		System.out.println(result2[i])
-		}
+
 		assert roboCrossover.onePointCrossover(result1, result2)
 	}
 	
 	def "And back to String"() {
 		when:
-		def string1 = "setAhead(90);  turnRight(360);  setBack(180);"
-		def string2 = "if(getEnergy()<getX()){ turnLeft(360); }  turnLeft(getVelocity());  ahead(90);  stop(); "
+		def string1 = "  setAhead(90);  turnRight(360);  setBack(180);  setTurnRight(90);  "
+		def string2 = "  if(getEnergy()<getX()){ turnLeft(360); }  turnLeft(getVelocity());  ahead(90);  stop();  "
 		def result1 = roboCrossover.makeArray(string1)
 		def result2 = roboCrossover.makeArray(string2)
 		
-		roboCrossover.onePointCrossover(result1,result2)
-		def rString1 = roboCrossover.backToString(result1)
-		def rString2 = roboCrossover.backToString(result2)
+		def rString1
+		def rString2
 		
 		then:
-		true
-		
+		for ( i in 0..5) {
+			items = roboCrossover.onePointCrossover(result1,result2)
+			result1 = items[0]
+			result2 = items[1]
+			rString1 = roboCrossover.backToString(result1)
+			rString2 = roboCrossover.backToString(result2)
+			//System.out.println("\n" + rString1 + "\n" + rString2)
+		}
 	}
 }
 
