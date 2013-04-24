fe33d8ffcd351d0333d03fed6c482685b9fbd542
mcgat003 <mcgat003@reliant.morris.umn.edu>
2013-04-24 13:41:17 -0500

    continuing on problem

--- EssentialsOfMetaheuristics/src/problems/Robocode.groovy
+++ EssentialsOfMetaheuristics/src/problems/Robocode.groovy
@@ -19,21 +19,41 @@ class Robocode {
 	def evolvedProperties = []
 	
 	def create() {
-		id = 
+		id = "12"
 		
+		for ( i in 0..6 ) {
 		movementFactory = new MovementFactory()
 		evolvedProperties[i] = movementFactory.returnRobot()
-	}
-	
-	def copy() {
+		}
+		codeRun = code[0]
+		codeWall = code[1];
+		codeCollision = code[2];
+		codeReceiveHit = code[3];
+		codeHit = code[4];
+		codeMiss = code[5];
+		
+		def values = ["id" : id, "codeRun" : codeRun,
+			"codeWall" : codeWall,
+			"codeCollision" : codeCollision,
+			"codeReceiveHit" : codeReceiveHit,
+			"codeHit" : codeHit,
+			"codeMiss" : codeMiss ]
 		
 	}
 	
-	def random() {
-		
-	}
+	def copy = { a -> a.clone() }
+	
+	def random = create
 	
 	def quality() {
+		robotBuilder = new RobotBuilder("templates/DarkSouls.template")
+		robotBuilder.buildJarFile(values)
+		
+		battleRunner = new BattleRunner("templates/battle.template")
+		
+		battleRunner.buildBattleFile(id)
+		
+		def score = battleRunner.runBattle(id)
 		
 	}
 	
